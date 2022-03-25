23db2fc39e1ec22a1e41fd0163dcedd8092be0ee
honey malhotra <honeymalhotra@developersinstitute.co.nz>
2022-03-25 20:02:20 +1300

    pagination applied on client side

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 const api = {
-  getProducts: async () =>
-    await fetch(`${process.env.REACT_APP_API_URL}/products`),
+  getProducts: async (currentPage) =>
+    await fetch(`${process.env.REACT_APP_API_URL}/products?page=${currentPage}`),
   getReports: async (accessToken) =>
     await fetch(`${process.env.REACT_APP_API_URL}/reports`, {
       headers: {
