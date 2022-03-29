487f40f3df3e06e258b3616ecf9d36212127135e
honey malhotra <honeymalhotra@developersinstitute.co.nz>
2022-03-30 12:28:11 +1300

    fixed pretteir and bugs for test failing issues on git hub

--- server/reports/report.router.js
+++ server/reports/report.router.js
@@ -5,24 +5,25 @@ const authorizationMiddleware = require("../middleware/authorizationMiddleware")
 const { checkJwt, checkScopes } = authorizationMiddleware;
 const errorHandlerMiddleware = require("../middleware/errorHandlerMiddleware");
 router.get(
-"/",
-checkJwt,
-checkScopes,
-errorHandlerMiddleware, 
-async (req, res, next) => {
-  try {
-    const categoryReport = await reportRepository.getCategoryReport();
-    const discountReport = await reportRepository.getDiscountReport();
+  "/",
+  checkJwt,
+  checkScopes,
+  errorHandlerMiddleware,
+  async (req, res, next) => {
+    try {
+      const categoryReport = await reportRepository.getCategoryReport();
+      const discountReport = await reportRepository.getDiscountReport();
 
-    const response = {
-      categoryReport,
-      discountReport,
-    };
+      const response = {
+        categoryReport,
+        discountReport,
+      };
 
-    return res.json(response);
-  } catch (err) {
-    next(err);
+      return res.json(response);
+    } catch (err) {
+      next(err);
+    }
   }
-});
+);
 
 module.exports = router;
